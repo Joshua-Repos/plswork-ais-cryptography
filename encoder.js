f8104c6c39f32d43490138eafc0ed475a4d00e7d
Joshua <105608879+Joshua-Repos@users.noreply.github.com>
2024-02-14 18:07:24 -0500

    Create encoder.js

---
+++ encoder.js
@@ -0,0 +1,117 @@
+let input = prompt("Enter your input")
+
+for (i = 0; i < input.length; i++) {
+    alert(input[i])
+}
+
+switch (input) {
+    case "a":
+        alert("b")
+        break
+    case "b":
+        alert("d")
+        break
+    case "c":
+        alert("f")
+        break
+    case "d":sss
+        alert("h")
+        break
+    case "e":
+        alert("j")
+        breaksss
+    case "f":
+        alert("l")
+        break
+    case "g":
+        alert("n")
+        break
+    case "h":
+        alert("p")
+        break
+    case "i":
+        alert("r")
+        break
+    case "j":
+        alert("t")
+        break
+    case "k":
+        alert("v")
+        break
+    case "l":
+        alert("x")
+        break
+    case "m":
+        alert("z")
+        break
+    case "n":
+        alert("y")
+        break
+    case "o":
+        alert("w")
+        break
+    case "p":
+        alert("u")
+        break
+    case "q":
+        alert("s")
+        break
+    case "r":
+        alert("q")
+        break
+    case "s":
+        alert("o")
+        break
+    case "t":
+        alert("m")
+        break
+    case "u":
+        alert("k")
+        break
+    case "v":
+        alert("i")
+        break
+    case "w":
+        alert("g")
+        break
+    case "x":
+        alert("e")
+        break
+    case "y":
+        alert("c")
+        break
+    case "z":
+        alert("a")
+        break
+    case "0":
+        alert("1")
+        break
+    case "1":
+        alert("3")
+        break
+    case "2":
+        alert("5")
+        break
+    case "3":
+        alert("7")
+        break
+    case "4":
+        alert("9")
+        break
+    case "5":
+        alert("8")
+        break
+    case "6":
+        alert("6")
+        break
+    case "7":
+        alert("4")
+        break
+    case "8":
+        alert("2")
+        break
+    case "9":
+        alert("0")
+        break
+
+}
